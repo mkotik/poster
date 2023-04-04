cd5bcfffc269ca7f2e2e5ef27e124a113841db94
Marat Kotik <mkotik97@gmail.com>
2023-04-03 20:05:37 -0400

    create stripe webhook endpoint

---
+++ pages/api/stripeWebhook.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+async function stripeWebhook(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    console.log(req);
+    res.send("in stripe webhook");
+  } else {
+    res.status(405).json({ message: "Method not allowed." });
+  }
+}
+
+export default stripeWebhook;
