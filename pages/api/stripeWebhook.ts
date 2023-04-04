8ccf746302d12c1f29deb2020022bd70d5c3d009
Marat Kotik <mkotik97@gmail.com>
2023-04-03 21:31:30 -0400

    tessst

--- pages/api/stripeWebhook.ts
+++ pages/api/stripeWebhook.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 async function stripeWebhook(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     console.log(req.body);
-    res.redirect("https://google.com/");
+    res.json({ received: true });
   } else {
     res.status(405).json({ message: "Method not allowed." });
   }
