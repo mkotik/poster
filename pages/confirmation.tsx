24c55c309e279b8a0b7d49ff8585235dbeb7dacb
Marat Kotik <mkotik97@gmail.com>
2023-04-11 20:08:41 -0400

    order confirmation page styling

--- pages/confirmation.tsx
+++ pages/confirmation.tsx
@@ -2,11 +2,13 @@ import Head from "next/head";
 import { GetServerSidePropsContext, NextPage } from "next";
 import { Base64 } from "js-base64";
 import { PaymentIntent } from "@stripe/stripe-js";
+import Footer from "../components/Footer";
 
 type ConfirmationProps = {
   data: PaymentIntent;
 };
 const Confirmation: NextPage<ConfirmationProps> = ({ data }) => {
+  console.log(data);
   return (
     <>
       <Head>
@@ -18,9 +20,21 @@ const Confirmation: NextPage<ConfirmationProps> = ({ data }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <p>Order Confirmed</p>
-        <p>{data.amount}</p>
+      <main className="flex flex-col m-auto  w-full sm:w-[500px] px-2  bg-stone-100 ">
+        <div className="min-h-screen pb-14">
+          <h3 className="mt-5 text-2xl font-semibold text-center">
+            Order Confirmed
+          </h3>
+          <div className="flex justify-between px-2 py-3 mt-5 bg-slate-200">
+            <p className="font-light">{data.description}</p>
+            <p className="font-light">{`$${(data.amount / 100).toFixed(2)}`}</p>
+          </div>
+          <p className="mt-5 font-medium text-center">
+            {` Order confirmation has been sent to ${data.receipt_email}`}
+          </p>
+          <p className="mt-16 font-medium text-center ">Happy Coding!</p>
+        </div>
+        <Footer />
       </main>
     </>
   );
