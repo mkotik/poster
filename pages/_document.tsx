294ffdb5c9f3bda9a50ea93b447b9b2749c15758
Marat Kotik <mkotik97@gmail.com>
2023-04-16 16:08:04 -0400

    Add GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RH7LRG6VXR"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-RH7LRG6VXR');
+        `}
+        </Script>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
