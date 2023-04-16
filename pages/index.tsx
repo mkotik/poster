ec5fbc465ac110ed25c25ffe57b4239f0450346e
Marat Kotik <mkotik97@gmail.com>
2023-04-16 15:57:24 -0400

    added og meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,23 @@ const Home: NextPage = () => {
         <title>React Developer Roadmap</title>
         <meta
           name="description"
-          content="Perfect for the self-taught React & Javascript Developer"
+          content="A Succinct Visual Guide for React Developers"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="React Developer Roadmap" />
+        <meta
+          property="og:description"
+          content="A Succinct Visual Guide for React Develoeprs"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://www.reactdeveloperroadmap.com/"
+        />
+        <meta property="og:image" content="/poster2.jpg" />
+        <meta property="og:image:width" content="1250" />
+        <meta property="og:image:height" content="834" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
