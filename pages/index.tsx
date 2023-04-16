6856a3383bdb964e21330d9c69b141b6c7cc6f52
Marat Kotik <mkotik97@gmail.com>
2023-04-16 18:20:03 -0400

    fix typescript err

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { NextPage } from "next";
-import Header from "../components/Header";
 import Footer from "../components/Footer";
 import MainContent from "../components/MainContent";
 
